9c224fb6e09f834c5dc6a6ddda87dc4a20e80e2e
BeniDiegoDev <benjamin.donofrio@outlook.fr>
2022-05-25 18:59:19 +0200

    fix load

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,19 @@ import { Link } from 'react-router-dom';
 // Import components
 import NavBar from "../components/NavBar";
 
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : 'red',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 function Home() {
   return (
     <div className="App">
@@ -22,6 +35,11 @@ function Home() {
         >
           BeniDiego.js
         </Link>
+        <Stack spacing={5}>
+          <Item>Item 1</Item>
+          <Item>Item 2</Item>
+          <Item>Item 3</Item>
+        </Stack>
       </header>
     </div>
   );
